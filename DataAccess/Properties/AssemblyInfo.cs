f6fbdd9b174c374af03eb9e91164a0c82697b2b2
Diana <dcma.92@gmail.com>
2020-04-20 15:55:58 -0500

    Librería de Acceso a base de datos SQL. Desarrollada en Visual Studio 2013 con .Net Framework 4.5. Actualizada con librería para logs

--- DataAccess/Properties/AssemblyInfo.cs
+++ DataAccess/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 // Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
 // para los componentes COM.  Si necesita obtener acceso a un tipo de este ensamblado desde 
 // COM, establezca el atributo ComVisible como true en este tipo.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
 [assembly: Guid("5c018ce3-4ecb-467c-be1a-57fc0303b840")]
